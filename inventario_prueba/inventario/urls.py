ceaf19cdb879cf217fb32a8127d8054003c7c8a4
lulaMariel <luciana.lavalle@gmail.com>
2024-11-28 12:04:53 -0300

    Mi primer proyecto con Django

--- inventario_prueba/inventario/urls.py
+++ inventario_prueba/inventario/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProductoListView, ProductoCreateView, ProductoUpdateView, ProductoDeleteView
+from .views import InicioView, ProductoListView, ProductoCreateView, ProductoUpdateView, ProductoDeleteView
 
 urlpatterns = [
-    path('', ProductoListView.as_view(), name='producto_list'),
+    path('', InicioView.as_view(), name="inicio"),
+    path('inicio/', InicioView.as_view(), name="inicio"),
+    path('listado/', ProductoListView.as_view(), name='producto_list'),
     path('agregar/', ProductoCreateView.as_view(), name='producto_create'),
     path('modificar/<pk>', ProductoUpdateView.as_view(), name='producto_update'),
     path('eliminar/<pk>', ProductoDeleteView.as_view(), name='producto_delete'),
--- inventario_prueba/inventario/views.py
+++ inventario_prueba/inventario/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
 from .models import Producto
 from .forms import ProductoForm
 
+class InicioView(TemplateView):
+    model = Producto
+    template_name = 'inicio_view.html'
+
 class ProductoListView(ListView):
     model = Producto
+    success_url= reverse_lazy('producto_list')
 
 class ProductoCreateView(CreateView):
     model = Producto
