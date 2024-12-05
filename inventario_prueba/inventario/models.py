d87eb7ad830b7d2f0bf3f263e8cca0228471caba
lulaMariel <luciana.lavalle@gmail.com>
2024-12-05 13:36:12 -0300

    cambio el str del modelo detallespedido

--- inventario_prueba/inventario/models.py
+++ inventario_prueba/inventario/models.py
@@ -25,3 +25,6 @@ class DetallesPedido(models.Model):
     pedido = models.ForeignKey(Pedido, on_delete = models.CASCADE)
     cantidad = models.PositiveIntegerField(null=True, blank=True)
     precio = models.DecimalField(max_digits = 10, decimal_places = 2, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.producto} {self.cantidad} {self.precio}"
